d0fdae8ad9c3a6334199b6fe57aea64d9259c20f
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-21 04:09:37 -0400

    Fixed Inherited Methods in Lease Contract Class

--- src/main/java/com/pluralsight/contract/LeaseContract.java
+++ src/main/java/com/pluralsight/contract/LeaseContract.java
@@ -8,33 +8,21 @@ public class LeaseContract extends Contract {
 
 
 
-
-
+    
+    // Inherited Methods
     @Override
     public double getTotalPrice() {
-        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
+        return (getVehicleSold().getPrice() - expectedEndingValue) + leaseFee;
     }
 
     @Override
     public double getMonthlyPayment() {
-        int numberOfPayments = 0;
-        double interestRate = 0;
-        if (financeOption) {
-            if (getVehicleSold().getPrice() >= 10000) {
-                numberOfPayments = 48;
-                interestRate = 4.25 / 1200;
-            } else {
-                numberOfPayments = 24;
-                interestRate = 5.25 / 1200;
-            }
-
-            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
-            monthlyPayment = Math.round(monthlyPayment * 100);
-            monthlyPayment /= 100;
-            return monthlyPayment;
-        } else {
-            return 0.0;
-        }
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
     }
 
 }
