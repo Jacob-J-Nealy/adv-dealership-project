424cf58ba5152bbdf76f25b0f3bb9fb554e85aa1
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-21 05:02:46 -0400

    Added try/catch to saveContract() Method

--- src/main/java/com/pluralsight/contract/ContractDataManager.java
+++ src/main/java/com/pluralsight/contract/ContractDataManager.java
@@ -1,9 +1,17 @@
 package com.pluralsight.contract;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class ContractDataManager {
 
     private void saveContract(Contract contract) {
 
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("contract.csv"));
+        } catch (Exception e) {
+            System.err.println("Could Not Store Contract");
+        }
 
     }
 }
